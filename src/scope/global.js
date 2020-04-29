0d51af8f9d99e6fa619d629d27ac8a19dabe587c
sntwandy <sntwandy@gmail.com>
2020-04-29 05:35:16 -0400

    Initializing the project, and learning about scope global in Vanilla JS

---
+++ src/scope/global.js
@@ -0,0 +1,12 @@
+var hello = 'Hello';
+let world = 'World';
+const helloWorld = `${hello} ${world}`;
+//console.log(helloWorld);
+
+const anotherFunction = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+}
+
+anotherFunction();
